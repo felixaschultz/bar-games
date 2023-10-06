06bd09533e9682cce2ed822cc74a71c2e437a08e
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-06 22:17:29 +0200

    Working on Websocket Integration

---
+++ js/websocket.js
@@ -0,0 +1,12 @@
+const url = "ws://www.felix-schultz.dk:8080/socket.php";
+const protocols = [8080];
+const webSocket = new WebSocket(url, protocols);
+
+webSocket.addEventListener("open", (event) => {
+    console.log("Connection established");
+    
+})
+
+webSocket.addEventListener("message", (event) => {
+    console.log("Message from server ", event.data);
+});
