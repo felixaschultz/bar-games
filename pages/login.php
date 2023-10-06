16871efba9dca41df7f6a05ecca38f0d31fcca0e
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-06 22:40:43 +0200

    Added more basic colors, and changed and changed themed to dark mode

--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,8 @@
 <!-- Login screen -->
 <article class="loginContainer">
     <form action="" method="post" class="login__screen">
-        <h1 class="login__title">Login</h1>
+        <h1 class="login__title">Choose username to get started</h1>
+        <p>Choose your name of choice to start a party game!</p>
         <label class="username-label" for="username">Username</label>
         <input class="form-input" type="text" name="username" id="username">
         <button type="submit" class="cta">Login</button>
