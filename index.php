9720fc443df46d56bf90055f602c9d4cf378281e
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-06 21:46:39 +0200

    Setup

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bar Games</title>
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/mobile.css">
+</head>
+<body>
+    <header class="main-header">
+
+    </header>
+    <main class="main-content">
+
+    </main>
+    <footer class="main-footer">
+        
+    </footer>
+    <script src="js/websocket.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
