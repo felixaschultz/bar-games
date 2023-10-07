d923e450c9b1e368ccd4dbee7f54a06ae5c6c5ec
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-07 20:32:59 +0200

    Placed success field fail field and items in a grid

--- pages/games/charades.php
+++ pages/games/charades.php
@@ -26,27 +26,29 @@
         }
         if(isset($_GET["theme"])){
         ?>
-        <section class="container">
-            <?php
-                foreach($themes as $theme){
-                    if($theme["theme"] == $_GET["theme"]){
-                        foreach($theme["items"] as $word){
-                            echo "<article draggable='true' ondragstart='drag(event)' class='game__item game__item--". $word["id"] ."' id='". $word["id"] ."'>";
-                            echo "<h2 class='game__item-title'>" . $word["title"] . "</h2>";
-                            echo "<p>" . $word["description"] . "</p>";
-                            echo "<p>Forbidden Words</p>";
-                            echo "<ul>";
-                            foreach($word["forbidden"] as $word){
-                                echo "<li>" . $word . "</li>";
+        <div class="grid g-3">
+            <section class="item-drop success" ondrop="drop(event)" ondragover="allowDrop(event)"></section>
+            <section class="container">
+                <?php
+                    foreach($themes as $theme){
+                        if($theme["theme"] == $_GET["theme"]){
+                            foreach($theme["items"] as $word){
+                                echo "<article draggable='true' ondragstart='drag(event)' class='game__item game__item--". $word["id"] ."' id='". $word["id"] ."'>";
+                                echo "<h2 class='game__item-title'>" . $word["title"] . "</h2>";
+                                echo "<p>" . $word["description"] . "</p>";
+                                echo "<p>Forbidden Words</p>";
+                                echo "<ul>";
+                                foreach($word["forbidden"] as $word){
+                                    echo "<li>" . $word . "</li>";
+                                }
+                                echo "</ul>";
+                                echo "</article>";
                             }
-                            echo "</ul>";
-                            echo "</article>";
                         }
                     }
-                }
-            ?>
-        </section>
-        <section class="item-drop success" ondrop="drop(event)" ondragover="allowDrop(event)"></section>
-        <section class="item-drop fail" ondrop="drop(event)" ondragover="allowDrop(event)"></section>
+                ?>
+            </section>
+            <section class="item-drop fail" ondrop="drop(event)" ondragover="allowDrop(event)"></section>
+        </div>
         <?php } ?>
 </main>
