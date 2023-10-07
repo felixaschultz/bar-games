0abe2717ffa5ec09a60164176ebb9de5b1f03a17
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-07 18:22:38 +0200

    Added themes to the game & theme items

--- game.php
+++ game.php
@@ -8,8 +8,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game: <?php echo $game ?></title>
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/mobile.css">
+    <script src="manifest.webmanifest"></script>
 </head>
 <body>
     <?php include("pages/games/" . $game . ".php") ?>
+    <script src="js/websocket.js"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
--- pages/games/schirades.php
+++ pages/games/schirades.php
@@ -1,10 +1,17 @@
 <?php
+    $root = $_SERVER["DOCUMENT_ROOT"];
     $games = json_decode(file_get_contents("games.json"), true);
-    
+    $themes = json_decode(file_get_contents($root."/schirades/themes.json"), true);
+
+    print_r($themes);
 ?>
 <header>
-    <h1><?php echo $_GET["game"]?></h1>
+    <h1 class="game-title"><?php echo $_GET["game"]?></h1>
 </header>
-<main>
-    
+<main class="content">
+    <?php
+        foreach($themes as $theme){
+            echo "<h2>" . $theme["title"] . "</h2>";
+        }
+    ?>
 </main>
